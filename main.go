c0345abc81c74748dfdbcc4f1ca84eb241678ff2
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-09-01 21:00:21 -0300

    wip

--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/lucassabreu/github-journaling-aggregator/github"
 )
 
 func main() {
-	fmt.Println(os.Getenv("GOPATH"))
+	if len(os.Args) == 1 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <username>", os.Args[0])
+		return
+	}
+
+	api := github.NewAPI(os.Args[1], os.Getenv("GITHUB_TOKEN"))
+	_, err := api.GetUser()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
 }
