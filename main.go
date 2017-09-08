da71b4aa4168f2db0bf60233aed914426013916e
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-09-07 21:21:21 -0300

    use go-github

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
-	"github.com/lucassabreu/github-journaling-aggregator/github"
+	"github.com/google/go-github/github"
 )
 
 func main() {
 	if len(os.Args) == 1 {
-		fmt.Fprintf(os.Stderr, "Usage: %s <username>", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s <pemfile>", os.Args[0])
 		return
 	}
 
-	api := github.NewAPI(os.Args[1], os.Getenv("GITHUB_TOKEN"))
-	_, err := api.GetUser()
+	client := NewGithubClient(os.Args[1], os.Getenv("GITHUB_TOKEN"))
+
+	opt := &github.RepositoryListOptions{Type: "owner", Sort: "updated", Direction: "desc"}
+	repos, _, err := client.Repositories.List(context.Background(), "", opt)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		return
+		log.Fatal(err)
 	}
+	fmt.Println(repos)
+}
+
+func NewGithubClient(username string, token string) *github.Client {
+	return github.NewClient(&http.Client{Transport: &TransportBasicAuth{
+		tr:       http.DefaultTransport,
+		username: username,
+		token:    token,
+	}})
+}
+
+type TransportBasicAuth struct {
+	tr       http.RoundTripper
+	username string
+	token    string
+}
+
+func (ba *TransportBasicAuth) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.SetBasicAuth(ba.username, ba.token)
+	return ba.tr.RoundTrip(req)
 }
