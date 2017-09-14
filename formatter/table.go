c16f63d99e86999ebb46ba4368bf9ac9002af5c5
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2017-09-13 22:54:32 -0300

    tables

--- formatter/table.go
+++ formatter/table.go
@@ -2,10 +2,12 @@ package formatter
 
 import (
 	"io"
+	"os"
 	"time"
 
 	"github.com/lucassabreu/github-journaling-aggregator/report"
 	"github.com/olekukonko/tablewriter"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 type Table struct {
@@ -16,34 +18,25 @@ type Table struct {
 }
 
 func NewGroupLineTable(w io.Writer) Table {
-	tw := tablewriter.NewWriter(w)
-	tw.SetHeader([]string{"Repository", "Created At", "What"})
-	tw.SetAutoMergeCells(true)
-	tw.SetRowLine(true)
-
-	return Table{
-		tw:       tw,
-		messages: make([]report.Message, 0),
-		errors:   make([]error, 0),
-	}
+	t := NewTable(w)
+	t.tw.SetHeader([]string{"Repository", "Created At", "What"})
+	t.tw.SetAutoMergeCells(true)
+	t.tw.SetRowLine(true)
+	return t
 }
 
 func NewMDTable(w io.Writer) Table {
-	tw := tablewriter.NewWriter(w)
-	tw.SetHeader([]string{"Repository", "Created At", "What"})
-	tw.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-	tw.SetCenterSeparator("|")
-
-	return Table{
-		tw:       tw,
-		messages: make([]report.Message, 0),
-		errors:   make([]error, 0),
-	}
+	t := NewTable(w)
+	t.tw.SetHeader([]string{"Repository", "Created At", "What"})
+	t.tw.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	t.tw.SetCenterSeparator("|")
+	return t
 }
 
 func NewTable(w io.Writer) Table {
+	tw := tablewriter.NewWriter(w)
 	return Table{
-		tw:       tablewriter.NewWriter(w),
+		tw:       tw,
 		messages: make([]report.Message, 0),
 		errors:   make([]error, 0),
 	}
@@ -52,14 +45,29 @@ func NewTable(w io.Writer) Table {
 func (t *Table) Close() {
 	t.sorter.Sort(t.messages)
 
+	var wM, wR int = 0, 0
 	t.tw.SetHeader([]string{"Repository", "Created At", "What"})
-	for _, m := range t.messages {
-		t.tw.Append([]string{
+	lines := make([][]string, len(t.messages))
+	for i, m := range t.messages {
+		if wM < len(m.Message) {
+			wM = len(m.Message)
+		}
+		if wR < len(*m.Repo.Name) {
+			wR = len(*m.Repo.Name)
+		}
+		lines[i] = []string{
 			m.CreatedAt.In(time.Local).Format("2006-01-02 15:04:05"),
 			*m.Repo.Name,
 			m.Message,
-		})
+		}
 	}
+
+	if width, _, err := terminal.GetSize(int(os.Stdin.Fd())); err == nil && width < wM {
+		wM = width - 30 - wR
+	}
+
+	t.tw.SetColWidth(wM)
+	t.tw.AppendBulk(lines)
 	t.tw.Render()
 }
 
